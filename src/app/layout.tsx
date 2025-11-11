f13f914ecad6cc0b71631659de12593b2b4dd99b
dev <romansyah26588@gmail.com>
2025-11-11 06:42:51 +0000

    updat wlangler.toml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   keywords: ["MoBoy", "Momo Coin", "Solana", "Web3", "Crypto", "Posting"],
   authors: [{ name: "MoBoy Team" }],
   icons: {
-    icon: "https://z-cdn-media.chatglm.cn/files/aff6c9b7-367e-4fb3-9f0e-a774972a1959_Moboy.png?auth_key=1762145266-55e52daac02d4fb5a64b008767ff9be6-0-4036d96b6c6c1cbefd88b1ecc8f8c2f7",
+    icon: "https://raw.githubusercontent.com/romansyah26588-stack/moboy-earn/refs/heads/main/public/Moboy.png",
   },
   openGraph: {
     title: "MoBoy - Posting About Momo Coin",
